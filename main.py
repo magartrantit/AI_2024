47d778235d354e70df78f7f769ba98161f12c2f8
Gabriel Danca <danca.gabriel@outlook.com>
2024-10-10 08:04:47 +0300

    Test

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+
+def main():
+    
+    dataframe1 = pd.read_excel("C:\\Users\\Gabriel\\Desktop\\College\\Artifial Intelligence\\Data cat personality and predation Cordonnier et al.xlsx")
+
+    # print(dataframe1)
+    
+    for column in dataframe1.columns:
+        missing_data = dataframe1[column].isnull()
+        
+        if missing_data.any():
+            print(f"Coloana '{column}' are date lipsă la următoarele rânduri:")
+            print(dataframe1[missing_data].index.tolist())
+        else:
+            print(f"Coloana '{column}' nu are date lipsă.")
+    
+    
+
+if __name__ == "__main__":
+    main()
