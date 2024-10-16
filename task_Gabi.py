723ce9f4040263c6094a3d5a460c3d39e1653567
DancaGabriel <157539222+DancaGabriel@users.noreply.github.com>
2024-10-16 11:10:40 +0300

    finished the 2 objectives

---
+++ task_Gabi.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import random as rd
+
+
+
+def main():
+    
+    dataframe1 = pd.read_excel("C:\\Users\\Gabriel\\Desktop\\College\\Artifial Intelligence\\Data cat personality and predation Cordonnier et al.xlsx")
+
+    sum = 0
+    count = 0
+    
+    reviews = []
+    count_reviews = 0
+        
+    for column in dataframe1.columns:  # Pasul 1: calculăm suma valorilor si numărul lor
+        if column == "Abondance":
+            print("Suntem aici")
+            for val in dataframe1[column]:
+               if(val != 'NSP'):
+                  sum = sum +  int(val)
+                  count += 1
+        
+    # print("Suma este: ", sum , " Iar count ul este: ", count)
+    final_number = round(sum / count,2)
+    # print(final_number)
+    
+    dataframe1["Abondance"] = dataframe1["Abondance"].replace("NSP", final_number) # Pasul 2: înlocuim NSP cu numărul dat de formulă
+    # print(dataframe1)
+    
+    
+    #Problema 2: Adăugăm review-uri aleatorii
+    
+    for column in dataframe1.columns:
+        if(column == "Plus"):
+            for val in dataframe1[column]:
+                if(len(str(val)) > 4):
+                    count_reviews += 1
+                    reviews.append(val)
+    
+    # print(count_reviews, " ", reviews[810])
+    
+    for column in dataframe1.columns:
+        if(column ==  "Plus"):
+           for i, val in dataframe1[column].items():
+               if(len(str(val)) < 4):
+                   random_number = rd.randint(0, count_reviews - 1)
+                   dataframe1.loc[i, column] = reviews[random_number]
+    
+    
+    print(dataframe1)
+                
+                    
+                    
+                    
+                    
+if __name__ == "__main__":
+    main()
